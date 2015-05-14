8fc831be74100174129e9d9030422180e57182ef
Michael <michael@evojam.com>
2015-05-14 15:11:04 +0200

    Specify ListCollectionsIterable
    Change-Id: I88cb109d1658556db46f0e3f546c817c981541bc

--- src/main/scala/com/evojam/mongodb/client/iterable/ListCollectionsIterable.scala
+++ src/main/scala/com/evojam/mongodb/client/iterable/ListCollectionsIterable.scala
@@ -1,5 +1,41 @@
 package com.evojam.mongodb.client.iterable
 
-trait ListCollectionsIterable[T] {
+import scala.concurrent.Future
 
+import com.evojam.mongodb.client.ObservableOperationExecutor
+import com.mongodb.ReadPreference
+import com.mongodb.async.AsyncBatchCursor
+import com.mongodb.operation.ListCollectionsOperation
+import org.bson.BsonDocument
+import org.bson.codecs.configuration.CodecRegistry
+import org.bson.conversions.Bson
+
+class ListCollectionsIterable[T](dbName: String, resultClass: Class[T], codecRegistry: CodecRegistry,
+  readPreference: ReadPreference, executor: ObservableOperationExecutor) extends MongoIterable[T] {
+
+  def filter(filter: Bson): ListCollectionsIterable[T] = ???
+
+  def maxTime(time: Long): ListCollectionsIterable[T] = ???
+
+  def batchSize(size: Int): ListCollectionsIterable[T] = ???
+
+  def batchCursor(): Future[AsyncBatchCursor[T]] = ???
+
+  private def execute(): MongoIterable[T] = ???
+
+  private def execute(operation: ListCollectionsOperation[T]): MongoIterable[T] = ???
+
+  private def createListCollectionsOperation(): ListCollectionsOperation[T] = ???
+
+  private def toBsonDocument(document: Bson): BsonDocument = ???
+
+  override def head(): Future[T] = ???
+
+  override def map[U](f: T => U): MappingIterable[T, U] = ???
+
+  override def headOpt = ???
+
+  override def cursor(batchSize: Option[Int]) = ???
+
+  override def foreach(f: (T) => Unit) = ???
 }
