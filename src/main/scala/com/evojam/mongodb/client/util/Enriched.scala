b426964c0fc2ada39a462a89878b3b0d43a2987e
Artur Bańkowski <artur.bankowski@gmail.com>
2015-05-14 21:32:35 +0200

    Make AsyncBatchCursor easly observable
    Change-Id: I899d1f70ab168d1086815f45827f7ba5006caca6

---
+++ src/main/scala/com/evojam/mongodb/client/util/Enriched.scala
@@ -0,0 +1,42 @@
+package com.evojam.mongodb.client.util
+
+import scala.collection.JavaConversions._
+
+import com.mongodb.async.{ AsyncBatchCursor, SingleResultCallback }
+import rx.lang.scala.Observable
+import rx.lang.scala.subjects.PublishSubject
+
+object Enriched {
+
+  implicit class AsyncBatchCursorEnriched[T](wrapped: AsyncBatchCursor[T]) {
+
+    def takeFirstAsObservable = {
+      wrapped.setBatchSize(1)
+
+      val observable = asObservable
+
+      observable.doOnNext(_ => wrapped.close())
+      observable.take(1)
+    }
+
+    def asObservable: Observable[T] = {
+
+      val subject = PublishSubject[T]()
+
+      wrapped.next(new SingleResultCallback[java.util.List[T]] {
+        override def onResult(result: java.util.List[T], t: Throwable) = {
+          if(t == null) {
+            Option(result)
+              .map(_.foreach(subject.onNext)) // FIXME: Better map to Unit than patternmatch
+              .getOrElse(subject.onCompleted())
+          } else {
+            subject.onError(t)
+          }
+        }
+      })
+
+      subject
+    }
+  }
+
+}
