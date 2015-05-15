b8a97a6ee4f76a184f3647f6acd9b997cd951db4
albert <albert@evojam.com>
2015-05-15 09:31:42 +0200

    Change MongoIterable foreach result type to Unit
    Change-Id: Iac8df80738ad5804e7652fdbf1cc3bd1af3412d5

--- src/main/scala/com/evojam/mongodb/client/iterable/MongoIterable.scala
+++ src/main/scala/com/evojam/mongodb/client/iterable/MongoIterable.scala
@@ -7,7 +7,7 @@ import rx.lang.scala.Observable
 trait MongoIterable[T] {
   def head: Future[T]
   def headOpt: Future[Option[T]]
-  def foreach(f: T => Unit): Future[Unit]
+  def foreach(f: T => Unit): Unit
   def map[U](f: T => U): MongoIterable[U]
   def cursor(batchSize: Option[Int] = None): Observable[T]
 }
