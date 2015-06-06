65685b44cd47226c1592edbbdc733108c8db51ae
albert <albert@evojam.com>
2015-06-06 23:29:57 +0200

    Create single index instead of multiple
    Change-Id: I498b5112005ee09282e16a425eb752438085db6d

--- src/test/scala/com/evojam/mongodb/client/integration/IndexSpec.scala
+++ src/test/scala/com/evojam/mongodb/client/integration/IndexSpec.scala
@@ -10,28 +10,19 @@ import com.evojam.mongodb.client.model.IndexModel
 import com.evojam.mongodb.client.util.IndexSpecSetup
 
 class IndexSpec extends Specification with IndexSpecSetup {
-
   "MongoDB Indexes" should {
     sequential
 
-    "create many single-key indexes" in {
-      val indexes = List(
-        IndexModel(new Document("testIndex1", 1), new IndexOptions()),
-        IndexModel(new Document("testIndex2", 1), new IndexOptions()),
-        IndexModel(new Document("testIndex3", 1), new IndexOptions()))
-
-      collection.createIndexes(indexes).flatMap(_ =>
-        collection.listIndexes().collect[Document]) must haveSize[List[Document]](4).await(10)
+    "create single-key index" in {
+      collection.createIndex(new Document("testIndex1", 1)).flatMap(_ =>
+        collection.listIndexes().collect[Document]) must haveSize[List[Document]](2).await(10)
     }
 
-    "create many multi-key indexes" in {
-      val indexes = List(
-        IndexModel(new Document("testIndex1", 1).append("testIndex2", 1).append("testIndex3", 1), new IndexOptions()),
-        IndexModel(new Document("testIndex4", 1).append("testIndex5", 1).append("testIndex6", 1), new IndexOptions()),
-        IndexModel(new Document("testIndex7", 1).append("testIndex8", 1).append("testIndex9", 1), new IndexOptions()))
+    "create multi-key index" in {
+      val index = new Document("testIndex1", 1).append("testIndex2", 1).append("testIndex3", 1)
 
-      collection.createIndexes(indexes).flatMap(_ =>
-        collection.listIndexes().collect[Document]) must haveSize[List[Document]](4).await(10)
+      collection.createIndex(index).flatMap(_ =>
+        collection.listIndexes().collect[Document]) must haveSize[List[Document]](2).await(10)
     }
 
     "drop a multi-key index by key names" in {
