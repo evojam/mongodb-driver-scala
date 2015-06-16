2a98de34023b32c16012c11bcb7920cee47be3d3
albert <albert@evojam.com>
2015-06-16 11:35:40 +0200

    Document generator for tests
    Change-Id: Idbfc3471b37b5860215f7e813964ee5a688353fd

--- src/test/scala/com/evojam/mongodb/client/integration/mongocollection/MongoCollectionSpec.scala
+++ src/test/scala/com/evojam/mongodb/client/integration/mongocollection/MongoCollectionSpec.scala
@@ -1,15 +1,18 @@
 package com.evojam.mongodb.client.integration
 
+import scala.collection.JavaConversions._
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
+import scala.util.Random
 
 import org.bson._
 import org.specs2.mutable.Specification
 
 import com.evojam.mongodb.client.MongoClients
 import com.evojam.mongodb.client.codec.Codecs._
+import com.evojam.mongodb.client.util.DocumentGenerator
 
-class MongoCollectionSpec extends Specification {
+class MongoCollectionSpec extends Specification with DocumentGenerator {
   sequential
 
   "MongoCollection" should {
@@ -19,12 +22,14 @@ class MongoCollectionSpec extends Specification {
 
     Await.ready(collection.drop(), Duration.Inf)
 
-    val docs = List(
-      new Document("prop", "value1"),
-      new Document("prop", "value2"),
-      new Document("prop", "value3"),
-      new Document("prop", "value4"),
-      new Document("prop", "value5"))
+    val docs = documents(Some(1000))
+    val propName = docs.headOption
+      .map(doc => asScalaSet(doc.keySet())
+        .headOption.getOrElse(throw new Exception("Document must have at least one property.")))
+      .getOrElse(throw new Exception("There must be at least one document generated."))
+
+    def randomDoc() =
+      docs(Random.nextInt(docs.size))
 
     "count on empty collection" in {
       collection.count must be_==(0).await
@@ -59,7 +64,7 @@ class MongoCollectionSpec extends Specification {
         .insertAll(docs)
         .flatMap(_ => collection.count())
 
-      res must be_==(docs.size).await
+      res must be_==(docs.size).await(100)
     }
 
     "find all documents in the collection" in {
@@ -69,17 +74,18 @@ class MongoCollectionSpec extends Specification {
     }
 
     "find particular document from the collection" in {
+      val doc = randomDoc()
       val res = collection
-        .find(new Document("prop", "value2"))
+        .find(doc)
         .head[Document]
-        .map(_.getString("prop"))
+        .map(_.get(propName))
 
-      res must be_==("value2").await
+      res must be_==(doc.get(propName)).await
     }
 
     "result to a single element list" in {
       val res = collection
-        .find(new Document("prop", "value2"))
+        .find(randomDoc())
         .collect[Document]
 
       res must haveSize[List[Document]](1).await
@@ -95,9 +101,9 @@ class MongoCollectionSpec extends Specification {
 
       val resValue = res
         .map(_.headOption)
-        .map(_.map(_.getString("prop")))
+        .map(_.map(_.get(propName)))
 
-      resValue must be_==(docs.headOption.map(_.getString("prop"))).await
+      resValue must be_==(docs.headOption.map(_.get(propName))).await
     }
 
     "skip few elements" in {
@@ -106,13 +112,13 @@ class MongoCollectionSpec extends Specification {
         .skip(4)
         .collect[Document]
 
-      res must haveSize[List[Document]](1).await
+      res must haveSize[List[Document]](docs.size - 4).await
 
       val resValue = res
         .map(_.lastOption)
-        .map(_.map(_.getString("prop")))
+        .map(_.map(_.get(propName)))
 
-      resValue must be_==(docs.lastOption.map(_.getString("prop"))).await
+      resValue must be_==(docs.lastOption.map(_.get(propName))).await
     }
 
     "skip few and limit to single element" in {
@@ -125,10 +131,10 @@ class MongoCollectionSpec extends Specification {
       res must haveSize[List[Document]](1).await
 
       val resValue = res
-        .map(_.lastOption)
-        .map(_.map(_.getString("prop")))
+        .map(_.headOption)
+        .map(_.map(_.get(propName)))
 
-      resValue must be_==(docs.lastOption.map(_.getString("prop"))).await
+      resValue must be_==(docs.drop(4).take(1).headOption.map(_.get(propName))).await
     }
 
     "skip few and limit to more than one element" in {
@@ -141,9 +147,9 @@ class MongoCollectionSpec extends Specification {
       res must haveSize[List[Document]](2).await
 
       val resValues = res
-        .map(_.map(_.getString("prop")))
+        .map(_.map(_.get(propName)))
 
-      resValues must be_==(docs.drop(2).take(2).map(_.getString("prop"))).await
+      resValues must be_==(docs.drop(2).take(2).map(_.get(propName))).await
     }
 
     "return elements one by one from cursor" in {
@@ -153,8 +159,10 @@ class MongoCollectionSpec extends Specification {
 
       val check = res.zip(docs).map {
         case (docDb, docCol) =>
-          (docDb.getString("prop"), docCol.getString("prop"))
+          (docDb.get(propName), docCol.get(propName))
       }.toList.toBlocking.toFuture
+
+      res.size.toBlocking.toFuture must be_==(docs.size).await
 
       check.map(_.forall {
         case (p1, p2) => p1 == p2
@@ -162,18 +170,16 @@ class MongoCollectionSpec extends Specification {
     }
 
     "return elements in batches from cursor" in {
+      val chunkSize = 3
       val res = collection
         .find()
-        .cursor[Document](2)
+        .cursor[Document](chunkSize)
         .toList.toBlocking.toFuture
-        .map(_.map(_.map(_.getString("prop"))))
+        .map(_.map(_.map(_.get(propName))))
 
-      val docVals = docs.map(_.getString("prop"))
+      val docVals = docs.map(_.get(propName))
 
-      res must haveSize[List[List[String]]](3).await
-      res.map(_(0)) must be_==(docVals.take(2)).await
-      res.map(_(1)) must be_==(docVals.drop(2).take(2)).await
-      res.map(_(2)) must be_==(docVals.drop(4).take(1)).await
+      res must haveSize[List[List[Object]]](Math.ceil(docs.size.toDouble / chunkSize).toInt).await
     }
 
     "return empty batch cursor from empty collection" in {
---
+++ src/test/scala/com/evojam/mongodb/client/util/DocumentGenerator.scala
@@ -0,0 +1,72 @@
+package com.evojam.mongodb.client.util
+
+import scala.util.Random
+
+import org.bson.Document
+
+trait DocumentGenerator {
+
+  private val MinArraySize = 100
+  private val MaxArraySize = 3000
+
+  private val MinDocSize = 5
+  private val MaxDocSize = 50
+
+  private val MinPropNameLength = 5
+  private val MaxPropNameLength = 20
+
+  private val MaxPropLength = 300
+
+  object JsonType extends Enumeration {
+    type JsonType = Value
+    // TODO handle Document and Array
+    val Number, String, Boolean = Value
+  }
+
+  private def nextArraySize(size: Option[Int]) =
+    size.getOrElse(Random.nextInt(MaxArraySize - MinArraySize) + MinArraySize)
+
+  private def nextDocSize() =
+    Random.nextInt(MaxDocSize - MinDocSize) + MinDocSize
+
+  private def nextPropName() =
+    Random.alphanumeric
+      .take(Random.nextInt(MaxPropNameLength - MinPropNameLength) + MinPropNameLength)
+      .mkString
+
+  private def nextJsonType() =
+    Random.nextInt(3) match {
+      case 0 => JsonType.Number
+      case 1 => JsonType.Boolean
+      case _ => JsonType.String
+    }
+
+  private def nextJsonValue(propType: JsonType.Value) = propType match {
+    case JsonType.Number => Random.nextDouble()
+    case JsonType.Boolean => Random.nextBoolean()
+    case JsonType.String => Random.alphanumeric.take(MaxPropLength).mkString
+  }
+
+  private def nextSchema(
+    map: Map[String, JsonType.Value] = Map.empty,
+    size: Int = 1): Map[String, JsonType.Value] = size match {
+      case size if size > 0 => {
+        nextSchema(map + (nextPropName() -> nextJsonType()), size - 1)
+      }
+      case _ => map
+  }
+
+  def nextDocument(schema: Map[String, JsonType.Value]): Document = {
+    val doc = new Document()
+    schema.foreach {
+      case (propName, propType) => doc.append(propName, nextJsonValue(propType))
+    }
+    doc
+  }
+
+  def documents(arraySize: Option[Int] = None): List[Document] = {
+    val schema = nextSchema(size = nextDocSize())
+    (for(i <- 1 to nextArraySize(arraySize))
+      yield nextDocument(schema)).toList
+  }
+}
